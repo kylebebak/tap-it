e740449c2107e269d90818137155c11ea488dfc5
Ftp User i3 Wingit <ftp_user@inbox.lv>
2013-09-08 12:20:03 +0100

    Adds some exemplary conventional code formatting to the Deck class.

--- bebak.kyle.tapit/src/com/bebak/kyle/tapit/Deck.java
+++ bebak.kyle.tapit/src/com/bebak/kyle/tapit/Deck.java
@@ -3,20 +3,44 @@ package com.bebak.kyle.tapit;
 import java.util.Collections;
 import java.util.Stack;
 
-
+/**
+ * Abstract structure, just a collection(stack) of Cards.
+ * AND AS WELL DECK RENDERING METHODS.
+ * 
+ * Also has some convenience methods and getters/setters.
+ * 
+ */
 public class Deck {
 
+  private final TapIt sketch;
+  private Stack<Card> cards;
+  
   /**
-	 * 
-	 */
-	private final TapIt sketch;
-private Stack<Card> cards;
-  private float x;
-  private float y;
+   * Contain which ?topleft position of the card?
+   * What is "the Origin" of the deck? Top left corner?
+   */
+  private float x, y;
 
+  /**
+   * Radius of the "card-circle" as percentage of the screen width.
+   */
   private float r = .22f; // * width
+  
   private final int border = Utils.color(0, 255, 255);
+  
+  /**
+   * Offset of each new card added to the deck (relative to previous card) as
+   * percentage of the screen width.
+   */
   private float cardThickness = .00085f; // * width
+  
+  /**
+   * Max number of ellipses which can be displayed.
+   * 
+   * When displaying deck on screen, we can only display
+   * a limited amount of ellipses. Thus we need to  define constant
+   * limiting amount of ellipses we can fit.
+   */
   private final int maxNumEllipses = 9;
   
   /**
@@ -26,9 +50,21 @@ private Stack<Card> cards;
    */
   private final String LOG = this.getClass().getSimpleName();  
 
-  // create an empty deck at the specified location
+  
+  /**
+   * Creates deck at specified location.
+   * 
+   * What exactly Deck is? Is it just "abstract" collection of cards?
+   * 
+   * @param tapIt reference to parent sketch
+   * @param x ?what is this? top left position?
+   * @param y
+   */
   public Deck(TapIt tapIt, float x, float y) {
     sketch = tapIt;
+    // TODO: First r is "percentage" and then it turns into pixels. Once variable should hold 
+    // 		 only one type of value. Should split it into two different variables: one constant for
+    //		 percentage and another actual pixel width.
 	r *= sketch.width;
     cardThickness *= sketch.width;
     
@@ -39,7 +75,16 @@ private Stack<Card> cards;
 
 
 
-  // add a card to the top of the deck
+  /**
+   * Add a card to the top of the deck and adjusts cards position
+   * to lay neatly on top of the deck.
+   * 
+   * Also advances XY coordinates of the deck. (Which means?)
+   * It's important to know that "c" card's location will be modified
+   * as the result of addition.
+   * 
+   * @param c
+   */
   public void addCard(Card c) {
     x += cardThickness;
     y += cardThickness;
@@ -48,21 +93,35 @@ private Stack<Card> cards;
     cards.push(c);
   }
 
-  // remove and return the top card
+  /**
+   * Remove and return the top card.
+   * 
+   * @return NULL if no cards available.
+   * 		
+   */
   public Card removeTop() {
+	// TODO: this seems to be a bug: if there's no cards to remove from top, the x,y 
+	//		 would still be reduced.
     x -= cardThickness;
     y -= cardThickness;
-    if (cards.size() == 0)
+    if (cards.size() == 0){
       return null;
+    }
     return cards.pop();
   }
 
-  // return the number of cards in the deck
+  /**
+   * Return the number of cards in the deck
+   * 
+   * @return
+   */
   public int getSize() {
     return cards.size();
   }
 
-  // shuffle the __remaining__ cards in the deck
+  /**
+   * Shuffle the __remaining__ cards in the deck
+   */
   public void shuffleDeck() {
     Collections.shuffle(cards);
     Stack<Card> newCards = new Stack<Card>();
@@ -72,24 +131,43 @@ private Stack<Card> cards;
       this.addCard(newCards.pop());
   }
 
-  // return the X coord of the top card in the deck
+  /**
+   * Return the X coord of the top card in the deck.
+   * 
+   * @return
+   */
   public float X() {
     return x;
   }
 
-  // return the Y coord of the top card in the deck
+  /**
+   * Return the Y coord of the top card in the deck.
+   * 
+   * @return
+   */
   public float Y() {
     return y;
   }
 
-  // change the location of the deck on the canvas
+  /** 
+   * Change the location of the deck on the canvas.
+   * 
+   * @param x
+   * @param y
+   */
   public void changePosition(float x, float y) {
     this.x = x;
     this.y = y;
   }
 
-  // return the index of symbol at the given x and y coordinates
-  // for the top card in the deck or -1 if no symbol at these coordinates
+  /**
+   * Return the index of symbol at the given x and y coordinates
+   * for the top card in the deck or -1 if no symbol at these coordinates.
+   * 
+   * @param x
+   * @param y
+   * @return
+   */
   public int indexAtPosition(float x, float y) {
     return cards.peek().indexAtPosition(x - this.x, y - this.y);
   }
@@ -97,7 +175,11 @@ private Stack<Card> cards;
 
 
 
-  // gives appearance of a stack of cards. THIS CAUSES SLOWDOWN IF TOO MANY ELLIPSES ARE DRAWN
+  /**
+   * Gives appearance of a stack of cards. 
+   * 
+   * THIS CAUSES SLOWDOWN IF TOO MANY ELLIPSES ARE DRAWN
+   */
   public void displayDeck() {
     sketch.fill(border);
     sketch.noSmooth(); // ellipses for cards will render faster
@@ -115,14 +197,18 @@ private Stack<Card> cards;
     sketch.smooth(); // make drawing smooth again
   }
 
-  // display the front of the top card by calling the Card method
+  /**
+   * Display the front of the top card by calling the Card method.
+   */
   public void displayTopFront() {
     if (cards.empty()) 
       return;
     cards.peek().displayFront();
   }
 
-  // display the back of the top card by calling the Card method
+  /**
+   * Display the back of the top card by calling the Card method.
+   */
   public void displayTopBack() {
     if (cards.empty())
       return;
